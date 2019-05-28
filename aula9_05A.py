872a435983df91c88ab51b4ac2ce81962df7345a
bernardbrandao <39226897+bernardbrandao@users.noreply.github.com>
2019-05-28 14:38:48 -0300

    Add files via upload

---
+++ aula9_05A.py
@@ -0,0 +1,35 @@
+from aula9pt2_ex3 import*
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import tstd
+
+def estatistica(x):
+    print("Desvio padrao populacional:  ", np.std(x))
+    print("Desvio padrao amostral: ", tstd(x))
+    print("Media: ", np.mean(x))
+    return np.std(x), tstd(x), np.mean(x)
+
+x, y, z = ler_arquivos('dados_alunos.txt')
+
+print("Figura 1: ")
+estatistica(x)
+print("Figura 2: ")
+estatistica(y)
+print("Figura 3: ")
+estatistica(z)
+
+bins_x = np.linspace(15, 45, 30)
+bins_y = np.linspace(1.5, 2, 30)
+bins_z = np.linspace(40, 120, 30)
+
+plt.figure(1)
+plt.hist(x, bins_x, histtype='bar', rwidth=0.8)
+plt.title('Primeira Coluna')
+plt.figure(2)
+plt.hist(y, bins_y, histtype='bar', rwidth=0.8)
+plt.title('Segunda Coluna')
+plt.figure(3)
+plt.hist(z, bins_z, histtype='bar', rwidth=0.8)
+plt.title('Terceira Coluna')
+plt.show()
+
